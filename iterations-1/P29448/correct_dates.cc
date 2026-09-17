ae04815cb4700df6bea20083a24a70d5bec21ae5
Pau Torres Valiente <pau.torres.valiente@estudiantat.upc.edu>
2026-09-17 18:51:30 +0200

    improving correct_dates

--- iterations-1/P29448/correct_dates.cc
+++ iterations-1/P29448/correct_dates.cc
@@ -1,34 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int day, month, year;
-    bool isLeap = false;
-    bool isCorrect = true;
 
-    cin >> day >> month >> year;
-
+bool isLeapYear (int year) {
     if (year % 4 == 0) {
         if (year % 100 != 0) {
-            isLeap = true;
+            return true;
         } else {
             year /= 100;
             if (year % 4 == 0) {
-                isLeap = true;
+                return true;
             }
         }
     }
-    if (year < 0) isCorrect = false;
-    if (month > 12 || month < 1) isCorrect = false;
-    if (month == 2) {
-        if (day < 1 || (isLeap ? day > 29 : day > 28)) isCorrect = false;
-    } else if (month == 4 || month == 6 || month == 9 || month== 11) {
-        if (day < 1 || day > 30) isCorrect = false;
-    } else {
-        if (day < 1 || day > 31) isCorrect = false;
-    }
-
-    cout << (isCorrect ? "Correct Date" : "Incorrect Date") << endl;
+    return false;
 }
 
-// Find the most efficient way
+bool isCorrect (int day, int month, int year) {
+    if (year < 0) return false;
+        if (month > 12 || month < 1) return false;
+        if (month == 2) {
+            if (day < 1 || (isLeapYear(year) ? day > 29 : day > 28)) return false;
+        } else if (month == 4 || month == 6 || month == 9 || month== 11) {
+            if (day < 1 || day > 30) return false;
+        } else {
+            if (day < 1 || day > 31) return false;
+    }
+    return true;
+}
+
+int main() {
+    int day, month, year;
+    
+    while (cin >> day >> month >> year) {
+        cout << (isCorrect(day, month, year) ? "Correct Date" : "Incorrect Date") << endl;
+    }
+}
---
+++ iterations-1/P97156/numbers_interval.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int val1, val2;
+
+    cin >> val1 >> val2;
+
+    for (int i = val1; i <= val2; i++) {
+        
+    }
+}
